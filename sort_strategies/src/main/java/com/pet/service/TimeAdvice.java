fe48169f1e855ad679a42d77c7a2f6c73e0716de
ThatLuckyDay <sergeiluckyday@gmail.com>
2025-02-07 14:51:10 +0600

    around advice

--- sort_strategies/src/main/java/com/pet/service/TimeAdvice.java
+++ sort_strategies/src/main/java/com/pet/service/TimeAdvice.java
@@ -1,15 +1,18 @@
 package com.pet.service;
 
 import lombok.NoArgsConstructor;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.aop.AfterReturningAdvice;
 import org.springframework.aop.MethodBeforeAdvice;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 @Component
 @NoArgsConstructor
-public class TimeAdvice implements MethodBeforeAdvice, AfterReturningAdvice {
+public class TimeAdvice implements MethodBeforeAdvice, AfterReturningAdvice, MethodInterceptor {
 
     @Override
     public void before(Method method, Object[] args, Object target) throws Throwable {
@@ -21,9 +24,14 @@ public class TimeAdvice implements MethodBeforeAdvice, AfterReturningAdvice {
     public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
         long time = System.nanoTime();
         System.out.println("Stop sort at: " + time);
+    }
 
-        if ("sort".equals(method.getName())) {
-            throw new RuntimeException("There should be a check here");
-        }
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        System.out.println("Start around");
+        int[] values = (int[]) invocation.proceed();
+        Arrays.stream(values != null ? values : new int[0]).forEach(elem -> System.out.print(elem + " "));
+        System.out.println("Stop around");
+        return values;
     }
 }
